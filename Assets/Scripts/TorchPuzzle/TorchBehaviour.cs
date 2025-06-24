7473c1f931726032a82370f105b97eab1aa99732
Icaka_XD <hristodincov@gmail.com>
2025-06-24 12:52:02 +0200

    Basic light puzzle logic+ erosion shader graph

--- Assets/Scripts/TorchPuzzle/TorchBehaviour.cs
+++ Assets/Scripts/TorchPuzzle/TorchBehaviour.cs
@@ -1,10 +1,33 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class TorchBehaviour : MonoBehaviour
+public class TorchBehaviour : MonoBehaviour //, IPointerDownHandler
 {
     public TorchManager TorchManager;
     public int TorchIndex;
+
+    //public void OnPointerDown(PointerEventData eventData)
+    //{
+    //    OnInteract();
+    //}
+
     private void OnMouseDown()
+    {
+        //OnInteract();
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        OnInteract();
+    }
+
+    //private void Update()
+    //{
+    //    if(Input.to)
+    //}
+
+    private void OnInteract()
     {
         TorchManager.OnClicked(TorchIndex);
     }
