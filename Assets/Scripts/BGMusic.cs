50566ed06aeaaf8e52cc24ab6283c504d3a2eb42
Icaka_XD <hristodincov@gmail.com>
2025-06-26 13:23:47 +0200

    Sound effects for moving stones + erosion+ ambient music added

---
+++ Assets/Scripts/BGMusic.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class BGMusic : MonoBehaviour
+{
+    public static BGMusic instance;
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
