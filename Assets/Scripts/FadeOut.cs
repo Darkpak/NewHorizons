1b9eceb3f8a0510ad6c6a844cf2fe6ba6c0501c7
Icaka_XD <hristodincov@gmail.com>
2025-07-01 15:58:16 +0200

    Transitions fix

--- Assets/Scripts/FadeOut.cs
+++ Assets/Scripts/FadeOut.cs
@@ -1,25 +1,45 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class FadeOut : MonoBehaviour
 {
     public Image panel;
     public float fadeOutSpeed;
-    public float devide =2;
-    private bool fadeOut =false;
+    public float devide = 2;
+    private bool fadeOut = false;
 
     private void Update()
     {
         if (fadeOut)
         {
+            Transition();
+            fadeOut = false;
+        }
+        
+    }
+
+    public void Transition()
+    {
+        StartCoroutine(TransitionIEnumerator());
+    }
+
+    private IEnumerator TransitionIEnumerator()
+    {
+        while (panel.color.a < 1)
+        {
             Color cutentColor = panel.color;
-            cutentColor.a += (fadeOutSpeed*Time.deltaTime)/devide;
+            cutentColor.a += (fadeOutSpeed * Time.deltaTime) / devide;
             panel.color = cutentColor;
+            yield return new WaitForEndOfFrame();
         }
     }
 
     private void OnMouseDown()
     {
+        print(fadeOut);
         fadeOut = true;
+        print(fadeOut);
     }
 }
